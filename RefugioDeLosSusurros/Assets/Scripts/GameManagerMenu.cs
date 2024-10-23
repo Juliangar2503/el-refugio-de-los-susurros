3437900e8973f525127112425dec5082fbc599bb
Julián Garrido <jgq325@gmail.com>
2024-10-23 18:09:41 +0200

    Añadir: Añadiendo funcionalidad cambiar escena

--- RefugioDeLosSusurros/Assets/Scripts/GameManagerMenu.cs
+++ RefugioDeLosSusurros/Assets/Scripts/GameManagerMenu.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
@@ -16,5 +16,21 @@ public class GameManagerMenu : MonoBehaviour
         
     }
 
-   
+    // Método para cambiar de escena
+    public void ChangeScene(string sceneName)
+    {
+        Debug.Log("Cambiando a la escena: " + sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Método para salir del juego
+    public void QuitGame()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+        // Esto no hará nada en el editor de Unity, pero funcionará en una build
+    }
+
+
+
 }
